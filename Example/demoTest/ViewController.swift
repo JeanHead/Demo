e1cce65027877c8f0a47134c6cfae4be439a91b4
彭磊 <jeanhead@126.com>
2019-07-02 16:22:15 +0800

    提交测试

---
+++ Example/demoTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  demoTest
+//
+//  Created by PengLeiGit on 07/02/2019.
+//  Copyright (c) 2019 PengLeiGit. All rights reserved.
+//
+
+import UIKit
+import demoTest
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.myColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ demoTest/Classes/ReplaceMe.swift
@@ -0,0 +1,4 @@
+import UIKit
+extension UIColor {
+    public static let myColor = UIColor.red
+}
